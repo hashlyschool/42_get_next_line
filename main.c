713d28bd7ad948186d1a390be70fd6e70ccbf423
hashlyschool <ilya12-141998@mail.ru>
2021-11-01 20:44:57 +0300

    mod main, fixed problen to norminette

--- main.c
+++ main.c
@@ -1,17 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hashly <hashly@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 20:44:17 by hashly            #+#    #+#             */
+/*   Updated: 2021/11/01 20:44:18 by hashly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <fcntl.h>
 #include "get_next_line.h"
 
-int	main()
+int	main(void)
 {
-	int		fd = open("file", O_RDONLY);
+	int		fd;
 	char	*line;
 	int		i;
 
-	while ((i = get_next_line(fd, &line)))
+	fd = open("file", O_RDONLY);
+	i = get_next_line(fd, &line);
+	while (i)
 	{
 		printf("i = %d %s\n", i, line);
 		free(line);
+		i = get_next_line(fd, &line);
 	}
 	printf("i = %d %s\n", i, line);
 	free(line);
